f11a3bbed82113a34e7b513439c6ec20a872ed76
matheus.martins <matheus.martins@ambevtech.com.br>
2025-05-02 09:19:14 -0300

    Remove .git interno do backend e unifica controle na raiz

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"]
+  }
+};
